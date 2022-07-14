1d42f5bc933c29f3741bbbbee74757edf37e886d
Justin Lee <justinlee@Justins-MacBook-Air.local>
2022-07-14 13:23:47 -0400

    Installing and running prettier - prettier found some weird closed tags that oddly the code did not catch

--- scripts/build.js
+++ scripts/build.js
@@ -4,20 +4,15 @@ const distFolder = './dist';
 
 // Clean-up
 if (fs.existsSync(distFolder)) {
-    fs.rmdirSync(distFolder, { recursive: true, force: true });
+	fs.rmdirSync(distFolder, { recursive: true, force: true });
 }
 
 fs.mkdirSync(distFolder);
 
-const folders = [
-    'components',
-    'resume',
-    'utils',
-];
+const folders = ['components', 'resume', 'utils'];
 
-const files = [
-    'index.html',
-    'index.css',
-];
+const files = ['index.html', 'index.css'];
 
-files.concat(folders).forEach(pattern => fs.copySync(pattern, `${distFolder}/${pattern}`));
+files
+	.concat(folders)
+	.forEach((pattern) => fs.copySync(pattern, `${distFolder}/${pattern}`));
--- utils/smooth-anchor-scroll.function.js
+++ utils/smooth-anchor-scroll.function.js
@@ -2,37 +2,37 @@
  * @param id (string) of the element to anchor scroll to.
  * @param options (object) configuring properties of the scroll (see below for more details)
  * @returns a promise resolving when the scroll is finished.
- * 
+ *
  * interface SmoothAnchorScrollOptions {
  *   msPerFrame: number; // Number of milliseconds per frame. Default is ~~16.7ms for 60fps.
  *   duration: number; // Duration in milliseconds of the scroll. Default is 300ms.
  * }
  */
 export function smoothAnchorScroll(id, options) {
-    const msPerFrame = options?.msPerFrame ?? (1000 / 60);
-    /** In milliseconds. */
-    const duration = options?.duration ?? 300;
-    const frames = duration / msPerFrame;
+	const msPerFrame = options?.msPerFrame ?? 1000 / 60;
+	/** In milliseconds. */
+	const duration = options?.duration ?? 300;
+	const frames = duration / msPerFrame;
 
-    let i = 1;
+	let i = 1;
 
-    const page = document.body;
-    const anchor = document.getElementById(id);
+	const page = document.body;
+	const anchor = document.getElementById(id);
 
-    const distance = anchor.getBoundingClientRect().y;
-    const scrollTopPerFrame = distance / frames;
+	const distance = anchor.getBoundingClientRect().y;
+	const scrollTopPerFrame = distance / frames;
 
-    return new Promise(resolve => {
-        const setIntervalId = setInterval(() => {
-            if (i >= frames) {
-                clearInterval(setIntervalId);
+	return new Promise((resolve) => {
+		const setIntervalId = setInterval(() => {
+			if (i >= frames) {
+				clearInterval(setIntervalId);
 
-                resolve();
-            }
+				resolve();
+			}
 
-            page.scrollBy({ top: scrollTopPerFrame });
+			page.scrollBy({ top: scrollTopPerFrame });
 
-            i = i + 1;
-        }, msPerFrame);
-    });
-};
+			i = i + 1;
+		}, msPerFrame);
+	});
+}
